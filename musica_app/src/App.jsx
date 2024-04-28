f397a9585e428587f15a4a040686ff3d50d8092f
Winchestre <solomonbassey20@gmail.com>
2024-04-28 16:11:20 -0700

    Updated 404 page

--- musica_app/src/App.jsx
+++ musica_app/src/App.jsx
@@ -8,20 +8,23 @@ import Profile from './pages/Profile'
 import Logout from './pages/Logout'
 import Albums from './pages/Albums/Albums'
 import AlbumDetail, { AlbumDetailLoader } from './pages/AlbumDetail'
+import NotFound from "./pages/NotFound";
 import TopCharts from './components/TopCharts'
 import RootLayout from './layouts/RootLayout'
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" element={<RootLayout />}>
-            <Route index element={<Home />} />
-            <Route path="radio" element={<Radio />} />
-            <Route path="collections" element={<Collections />} />
-            <Route path="videos" element={<Videos />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="logout" element={<Logout />} />
-            <Route path="albums" element={<Albums />} />
-            <Route path="api/albums/:id" element={<AlbumDetail />} loader={AlbumDetailLoader} />
+        <Route path="/" element={<RootLayout />} errorElement={<NotFound />}>
+            <Route errorElement={<NotFound />}>
+                <Route index element={<Home />} />
+                <Route path="radio" element={<Radio />} />
+                <Route path="collections" element={<Collections />} />
+                <Route path="videos" element={<Videos />} />
+                <Route path="profile" element={<Profile />} />
+                <Route path="logout" element={<Logout />} />
+                <Route path="albums" element={<Albums />} />
+                <Route path="api/albums/:id" element={<AlbumDetail />} loader={AlbumDetailLoader} />
+            </Route>
         </Route>
         )
     )
