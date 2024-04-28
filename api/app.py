dd4243159485ceb42eea393b0a7c7ce31509ed36
Winchestre <solomonbassey20@gmail.com>
2024-04-28 12:10:57 -0700

    index routing v1

--- api/app.py
+++ api/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='', static_folder='../musica_app/dist', template_folder='../musica_app/dist')
+app = Flask(__name__,
+    static_url_path=",
+    static_folder='../musica_app/dist',
+    template_folder='../musica_app/dist')
 CORS(app)
 
 album_data = {
