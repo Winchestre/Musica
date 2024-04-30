35717d239a377cc85c469e45953ed5adf02b18ad
Winchestre <solomonbassey20@gmail.com>
2024-04-30 15:30:55 -0700

    Refactored folder structure

--- api/app.py
+++ api/app.py
@@ -18,12 +18,12 @@ album_data = {
                 {
                     "id": 1,
                     "title": "United In Grief",
-                     "image_url": '/static/images/Rectangle 15.png',
+                    "image_url": '/static/images/Rectangle 15.png',
                 },
                 {
                     "id": 2,
                     "title": "N95",
-                     "image_url": '/static/images/Rectangle 27.png',
+                    "image_url": '/static/images/Rectangle 27.png',
                 },
                 {
                     "id": 3,
@@ -33,12 +33,12 @@ album_data = {
                 {
                     "id": 4,
                     "title": "Die Hard",
-                     "image_url": '/static/images/Rectangle 21.png',
+                    "image_url": '/static/images/Rectangle 21.png',
                 },
                 {
                     "id": 5,
                     "title": "Father Time",
-                     "image_url": '/static/images/Rectangle 16.png',
+                    "image_url": '/static/images/Rectangle 16.png',
                 },
                 {
                     "id": 6,
@@ -48,12 +48,12 @@ album_data = {
                 {
                     "id": 7,
                     "title": "Rich Spirit",
-                     "image_url": '/static/images/Rectangle 15.png',
+                    "image_url": '/static/images/Rectangle 15.png',
                 },
                 {
                     "id": 8,
                     "title": "We Cry Together",
-                     "image_url": '/static/images/Rectangle 14.png',
+                    "image_url": '/static/images/Rectangle 14.png',
                 },
                 {
                     "id": 9,
@@ -63,12 +63,12 @@ album_data = {
                 {
                     "id": 10,
                     "title": "Count Me Out",
-                     "image_url": '/static/images/Rectangle 14 (2).png',
+                    "image_url": '/static/images/Rectangle 14 (2).png',
                 },
                 {
                     "id": 11,
                     "title": "Crown",
-                     "image_url": '/static/images/Rectangle 14.png',
+                    "image_url": '/static/images/Rectangle 14.png',
                 },
                 {
                     "id": 12,
@@ -78,15 +78,15 @@ album_data = {
                 {
                     "id": 13,
                     "title": "Saviour (Interlude)",
-                     "image_url": '/static/images/Rectangle 14 (2).png',
+                    "image_url": '/static/images/Rectangle 14 (2).png',
                 },
                 {
                     "id": 14,
                     "title": "Saviour",
-                     "image_url": '/static/images/Rectangle 14.png',
+                    "image_url": '/static/images/Rectangle 14.png',
                 },
                 {
-                    "id":15,
+                    "id": 15,
                     "title": "Aunty Diaries",
                     "image_url": '/static/images/Rectangle 14 (1).png',
                 },
@@ -98,15 +98,15 @@ album_data = {
                 {
                     "id": 17,
                     "title": "Mother Sober",
-                     "image_url": '/static/images/Rectangle 14 (2).png',
+                    "image_url": '/static/images/Rectangle 14 (2).png',
                 },
                 {
                     "id": 18,
                     "title": "Mirror",
-                     "image_url": '/static/images/Rectangle 14.png',
+                    "image_url": '/static/images/Rectangle 14.png',
                 },
                 {
-                    "id":19,
+                    "id": 19,
                     "title": "The Heart Part 5",
                     "image_url": '/static/images/Rectangle 14 (1).png',
                 },
@@ -124,22 +124,22 @@ album_data = {
                 {
                     "id": 1,
                     "title": "First Person Shooter",
-                     "image_url": '/static/images/Rectangle 29.png',
+                    "image_url": '/static/images/Rectangle 29.png',
                 },
                 {
                     "id": 2,
                     "title": "Bahamas Promises",
-                     "image_url": '/static/images/Rectangle 17.png',
+                    "image_url": '/static/images/Rectangle 17.png',
                 },
                 {
                     "id": 3,
                     "title": "Slime You Out",
-                     "image_url": '/static/images/Lead-image.png',
+                    "image_url": '/static/images/Lead-image.png',
                 },
                 {
                     "id": 4,
                     "title": "Drew A Picasso",
-                    "description":"Just like Picasso",
+                    "description": "Just like Picasso",
                     "image_url": '/static/images/Rectangle 14 (5).png',
 
                 },
@@ -157,18 +157,18 @@ album_data = {
                 {
                     "id": 1,
                     "title": "The Pressure",
-                     "image_url": '/static/images/Rectangle 15.png',
+                    "image_url": '/static/images/Rectangle 15.png',
 
                 },
                 {
                     "id": 2,
                     "title": "Death Row East",
-                     "image_url": '/static/images/Rectangle 29.png',
+                    "image_url": '/static/images/Rectangle 29.png',
                 },
                 {
                     "id": 3,
                     "title": "40 side",
-                     "image_url": '/static/images/Rectangle 16.png',
+                    "image_url": '/static/images/Rectangle 16.png',
                 },
                 {
                     "id": 4,
@@ -188,18 +188,18 @@ album_data = {
                 {
                     "id": 7,
                     "title": "Store Run",
-                     "image_url": '/static/images/Rectangle 26.png',
+                    "image_url": '/static/images/Rectangle 26.png',
 
                 },
                 {
                     "id": 8,
                     "title": "Moments",
-                     "image_url": '/static/images/Rectangle 29.png',
+                    "image_url": '/static/images/Rectangle 29.png',
                 },
                 {
                     "id": 9,
                     "title": "Nobody (feat. Ms. Lauryn Hill)",
-                     "image_url": '/static/images/Rectangle 14 (5).png',
+                    "image_url": '/static/images/Rectangle 14 (5).png',
                 },
                 {
                     "id": 10,
@@ -357,24 +357,33 @@ new_releases = {
     ]
 }
 
+
+@app.route("/")
+def index():
+    return "Welcome to my API"
+
+
 @app.route("/api/albums", methods=['GET'])
 def albumItems():
     if "albums" in album_data:
         return jsonify(album_data["albums"])
     return jsonify({"error": "Not Found"}), 404
 
+
 @app.route("/api/albums/<int:id>", methods=['GET'])
-def albumItem(id):
+def albumItem(user_id):
     if "albums" in album_data:
         for album in album_data["albums"]:
-            if "id" in album and album["id"] == id:
+            if "id" in album and album["id"] == user_id:
                 return album
         return jsonify({"error": "No ID found"}), 404
     return False
 
+
 @app.route("/api/collections", methods=['GET'])
 def collections():
     return jsonify(collections_data)
+
 
 @app.route("/api/new_releases", methods=['GET'])
 def newReleases():
